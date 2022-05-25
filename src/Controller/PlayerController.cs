d5c6f12bfbb3fe91e74217dbdeb4f60aeb12ef0c
eskevv <hkeven89@gmail.com>
2022-05-24 20:49:39 -0400

    refactoring

--- src/Controller/PlayerController.cs
+++ src/Controller/PlayerController.cs
@@ -1,58 +1,47 @@
 public class PlayerController : IController
 {
-   public PlayerController() { }
-
    // methods 
-   private Character? get_target(List<Character> party) {
+   private Character? targets_menu(List<Character> party) {
       var targets = new string[party.Count];
       for (int x = 0; x < targets.Length; x++) {
          targets[x] = party[x].Name;
       }
-      var targets_menu = new Menu(targets, ConsoleColor.DarkRed, "*** { SELECT A TARGET } ***");
 
-      while (true) {
-         int item_num = targets_menu.collect_input();
-         if (item_num != -1) {
-            return party[item_num];
-         }
-         return null;
-      }
+      var targets_menu = new Menu(targets, ConsoleColor.DarkRed, "*** { SELECT A TARGET } ***");
+      int item_num = targets_menu.collect_input();
+      if (item_num == -1) return null;
+      return party[item_num];
+
    }
 
-   private ISpecial? get_special_moves(List<ISpecial> specials) {
+   private ISpecial? specials_menu(List<ISpecial> specials) {
       var special_moves = new string[specials.Count];
       for (int x = 0; x < special_moves.Length; x++) {
          special_moves[x] = specials[x].Name;
       }
+      
       var specials_menu = new Menu(special_moves, ConsoleColor.DarkMagenta, "*** { SPECIALS } ***");
-
-      while (true) {
-         int item_num = specials_menu.collect_input();
-         if (item_num != -1) {
-            return specials[item_num];
-         }
-         return null;
-      }
+      int item_num = specials_menu.collect_input();
+      if (item_num == -1) return null;
+      return specials[item_num];
    }
 
    private AttackAction? get_attacking_action(Character c, List<Character> enemies) {
-      Character? target = get_target(enemies);
-      if (target != null) {
-         return new AttackAction(c, target);
-      }
-      return null;
-   } 
+      Character? target = targets_menu(enemies);
+      if (target == null) return null;
+      
+      return new AttackAction(c, target);
+   }
 
    private SpecialAction? get_special_action(Character c, List<Character> enemies) {
       while (true) {
-         ISpecial? special = get_special_moves(c.Specials);
-         if (special != null) {
-            Character? new_target = get_target(enemies);
-            if (new_target != null) {
-               return new SpecialAction(c, new_target, special);
-            }
-         }
+         ISpecial? special = specials_menu(c.Specials);
          if (special == null) return null;
+
+         Character? new_target = targets_menu(enemies);
+         if (new_target == null) continue;
+
+         return new SpecialAction(c, new_target, special);
       }
    }
 
